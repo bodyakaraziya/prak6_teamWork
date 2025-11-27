d7b84bf15bab855552db136e3bbab0baf8b8929d
Bohdan Karaziia <bodyakaraziya1@gmail.com>
2025-11-27 14:06:36 +0200

    add bohdan card

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+              <img src={"/bohdan.png"} alt="Bohdan Karaziia" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Bohdan Karaziia</h2>
+          <p style={styles.position}>QA</p>
         </div>
       </div>
     </main>
